fc94bd6c360a23a8cbe509bcb38de588c147db4e
hongthiet1994 <hongthiet1994@gmail.com>
2022-01-15 23:09:39 +0700

    esp8266 and dht11

--- esp8266_dht11/src/main.cpp
+++ esp8266_dht11/src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <DHT.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+const int DHTPIN = D4;
+const int DHTTYPE = DHT11;
+DHT dht(DHTPIN, DHTTYPE);
+
+byte degree[8] = {
+  0B01110,
+  0B01010,
+  0B01110,
+  0B00000,
+  0B00000,
+  0B00000,
+  0B00000,
+  0B00000
+};
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.init();  
+  lcd.backlight();
+  lcd.print("Temper: ");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.createChar(1, degree);
+  dht.begin();  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(t) || isnan(h)) {
+  } 
+  else 
+  {
+    lcd.setCursor(10,0);
+    lcd.print(round(t));
+    lcd.print(" ");
+    lcd.write(1);
+    lcd.print("C");
+
+    lcd.setCursor(10,1);
+    lcd.print(round(h));
+    lcd.print(" %");    
+  }
 }
